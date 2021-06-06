1c12169d82c628a8d490d274b8e43322d0da7967
Glenroy Little <glittle02@knights.ucf.edu>
2021-06-06 00:31:45 -0500

    Final Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,5 +1,9 @@
 package base;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Glenroy Little
+ */
+import java.util.Scanner;
 /**
  Exercise 10 - Self-Checkout
  Working with multiple inputs and currency can introduce some tricky precision issues.
@@ -30,8 +34,61 @@ package base;
  */
 public class App 
 {
+    static Scanner in = new Scanner(System.in);
+    static final double TAX_RATE = .055;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App myApp = new App();
+
+        Integer i1 = getprice(1);
+        Integer q1 = getquantity(1);
+        Integer i2 = getprice(2);
+        Integer q2 = getquantity(2);
+        Integer i3 = getprice(3);
+        Integer q3 = getquantity(3);
+
+        float subtotal = myApp.calcsubtotal(i1, q1, i2, q2, i3, q3);
+        double tax = myApp.calctax(subtotal);
+        double total = myApp.findtotal(subtotal, tax);
+
+        myApp.printtotals(subtotal, tax, total);
+    }
+
+    public void printtotals(float subtotal, double tax, double total)
+    {
+        System.out.printf("Subtotal: $%.2f%nTax: $%.2f%nTotal: $%.2f", subtotal, tax, total);
+    }
+
+    public double findtotal(float subtotal, double tax)
+    {
+        double total = subtotal + tax;
+        return total;
+    }
+
+    public double calctax(float subtotal)
+    {
+        double tax = subtotal * TAX_RATE;
+        return tax;
+    }
+
+    public float calcsubtotal(Integer i1, Integer q1, Integer i2, Integer q2, Integer i3, Integer q3)
+    {
+        float sum = i1 * q1 + i2 * q2 + i3 * q3;
+        return sum;
+    }
+
+    public static Integer getquantity(int num)
+    {
+        System.out.print("Enter the quantity of item " + num + ": ");
+        Integer quantity = in.nextInt();
+        return quantity;
+    }
+
+    public static Integer getprice(int num)
+    {
+        System.out.print("Enter the price of item " + num + ": ");
+        Integer price = in.nextInt();
+        return price;
     }
 }
